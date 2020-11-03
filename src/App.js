4f8ea74a0c5dd5124f3cf8bfbc26a9d09ff233c0
shaileechoxi <shaileechoxi@gmail.com>
2020-11-03 14:26:23 -0500

    app.js modified

--- src/App.js
+++ src/App.js
@@ -8,26 +8,13 @@ import SaveIcon from '@material-ui/icons/Save';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <ButtonGroup>
-          <Button onClick={()=> alert("You Saved me")} startIcon = {<SaveIcon />} size="large" variant="contained" color="primary" >Save</Button>
-          <Button onClick={()=> alert("You Deleted me")} startIcon = {<DeleteIcon />} size="large" variant="contained" color="secondary" >Delete</Button>
-        </ButtonGroup>     
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonGroup>
+        <Button onClick={()=> alert("You Saved me")} startIcon = {<SaveIcon />} size="large" variant="contained" color="primary" >Save</Button>
+        <Button onClick={()=> alert("You Deleted me")} startIcon = {<DeleteIcon />} size="large" variant="contained" color="secondary" >Delete</Button>
+      </ButtonGroup> 
     </div>
   );
 }
